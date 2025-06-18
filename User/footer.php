23ce0ff5426b80ca599a49c5fe45f815e0bade17
SalshaNabilaa <152708506+SalshaNabilaa@users.noreply.github.com>
2025-06-18 23:38:29 +0700

    Add files via upload

---
+++ User/footer.php
@@ -0,0 +1,19 @@
+<style>
+    .footer {
+        background-color: #f1f1f1;
+        padding: 15px 0;
+        font-size: 14px;
+        text-align: center;
+        width: 100%;
+    }
+
+    .container {
+        width: 90%;
+        margin: 0 auto;
+    }
+</style>
+
+<footer class="text-center py-3 mt-4" style="background-color: #f1f1f1;">
+  <p class="mb-0">© 2025 Semua Hak Dilindungi.</p>
+</footer>
+    
