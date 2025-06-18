791d6708ff0f9256ec3194b144911162ff9568db
Nafisya Yagtias <151293824+nafisyaa@users.noreply.github.com>
2025-06-18 23:37:38 +0700

    Update db.php

--- Admin/db.php
+++ Admin/db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "projectwebtfixbgt";
+$db = "projekwebtfix";
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
